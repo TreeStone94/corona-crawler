de53b7aabd80146938d52c6ebea1e76eb87563a7
남우석 <nws94@douzone.com>
2023-05-31 02:36:57 +0900

    크롤링 예제

---
+++ examples/http-api.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+const main = async () => {
+    const resp = await axios.get('https://yjiq150.github.io/coronaboard-crawling-sample/example-data.json');
+
+    console.log(resp.data.content);
+}
+
+main();
